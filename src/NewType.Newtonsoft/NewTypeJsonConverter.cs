5a886f4f3b7389777b22563e28e0a88de8de51b7
mark-pro <20671988+mark-pro@users.noreply.github.com>
2025-01-19 17:05:09 -0500

    Added icon for future nuget package publishing
    Renamed benchmarks project to match assembly name (needed for benchmark
    dotnet)

--- src/NewType.Newtonsoft/NewTypeJsonConverter.cs
+++ src/NewType.Newtonsoft/NewTypeJsonConverter.cs
@@ -5,7 +5,7 @@ namespace Oneiro;
 
 public sealed class NewTypeJsonConverter : JsonConverter {
 
-    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) {
         if (value is null) {
             writer.WriteNull();
             return;
@@ -22,7 +22,7 @@ public sealed class NewTypeJsonConverter : JsonConverter {
     public override object ReadJson(
         JsonReader reader,
         Type objectType,
-        object existingValue,
+        object? existingValue,
         JsonSerializer serializer
     ) {
         var typeToConvert = objectType.BaseType!.GetGenericArguments();
@@ -31,7 +31,7 @@ public sealed class NewTypeJsonConverter : JsonConverter {
         var converter = Activator.CreateInstance(typeConverter, []);
         var readJson = typeConverter.GetMethod("ReadJson", [typeof(JsonReader), typeof(Type), objectType, typeof(bool), typeof(JsonSerializer)]);
         var convertedValue = readJson!.Invoke(converter, [reader, objectType, existingValue, false, serializer]);
-        return convertedValue;
+        return convertedValue!;
     }
     
     public override bool CanConvert(Type objectType) =>
@@ -42,7 +42,7 @@ public sealed class NewTypeJsonConverter<T, TNewType> : JsonConverter<TNewType>
     where TNewType : NewType<T, TNewType>
     where T : struct {
     
-    public override TNewType ReadJson(JsonReader reader, Type objectType, TNewType existingValue, bool hasExistingValue, JsonSerializer serializer) {
+    public override TNewType ReadJson(JsonReader reader, Type objectType, TNewType? existingValue, bool hasExistingValue, JsonSerializer serializer) {
         if(reader.TokenType == JsonToken.Null) {
             return NewType<T, TNewType>.Default;
         }
@@ -50,7 +50,7 @@ public sealed class NewTypeJsonConverter<T, TNewType> : JsonConverter<TNewType>
         return NewType<T, TNewType>.From(value);
     }
 
-    public override void WriteJson(JsonWriter writer, TNewType value, JsonSerializer serializer) {
-        serializer.Serialize(writer, value.GetValue());
+    public override void WriteJson(JsonWriter writer, TNewType? value, JsonSerializer serializer) {
+        serializer.Serialize(writer, value?.GetValue() ?? default(T));
     }
 }
